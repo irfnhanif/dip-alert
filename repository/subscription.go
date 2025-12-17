fbafd3b1b0d5ff27d9a9db2ab31916ba1f8b3670
irfnhanif <14.irfanh@gmail.com>
2025-12-17 13:36:22 +0700

    refactor: restructure SubscriptionRepository methods for improved error handling and clarity

--- repository/subscription.go
+++ repository/subscription.go
@@ -3,52 +3,118 @@ package repository
 import (
 	"database/sql"
 	"dip-alert/model"
-	"log"
+	"errors"
 )
 
-func StoreSubscription(db *sql.DB, subscription model.Subscription) {
-	stmt, err := db.Prepare(`INSERT INTO subscriptions(symbol, category, look_back_days, trigger_limit, last_alerted_at) VALUES(?, ?, ?)`)
-    if err != nil {
-        log.Fatal(err)
-    }
-	defer stmt.Close()
-
-	_, err = stmt.Exec(subscription.Symbol, subscription.Category, subscription.LookBackDays, subscription.TriggerLimit, subscription.LastAlertedAt)
-	if err != nil {
-        log.Fatal(err)
-    }
+type SubscriptionRepository interface {
+	Create(createSubReq model.CreateSubscriptionRequest) error
+	FindAll() ([]*model.Subscription, error)
+	FindBySymbol(symbol string) (*model.Subscription, error)
+	Delete(symbol string) error
 }
 
-func ListSubscriptions(db *sql.DB) *sql.Rows  {
-	stmt, err := db.Prepare("SELECT * FROM subscriptions")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer stmt.Close()
-
-	subscriptions, err := stmt.Query()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return subscriptions
+type SqliteSubscriptionRepository struct {
+	db *sql.DB
 }
 
-func DeleteSubscription(db *sql.DB, ID int)  {
-	var exist int
-    err := db.QueryRow("SELECT 1 FROM subscriptions WHERE id = ?", ID).Scan(&exist)
-    if err != sql.ErrNoRows {
-        return 
-    }
+func NewSqliteSubscriptionRepository(db *sql.DB) *SqliteSubscriptionRepository {
+	return &SqliteSubscriptionRepository{db: db}
+}
+
+func (r *SqliteSubscriptionRepository) Create(createSubReq *model.CreateSubscriptionRequest) error {
+	query := `
+	INSERT INTO subscriptions(symbol, category, look_back_days, trigger_limit) 
+		VALUES(?, ?, ?, ?)
+	`
+	_, err := r.db.Exec(query, createSubReq.Symbol, createSubReq.Category, createSubReq.LookBackDays, createSubReq.TriggerLimit)
+
+	return err
+}
+
+func (r *SqliteSubscriptionRepository) FindAll() ([]*model.Subscription, error) {
+	query := `
+	SELECT id, symbol, category, look_back_days, trigger_limit, last_alerted_at, created_at 
+	FROM subscriptions
+	ORDER BY category DESC AND symbol ASC
+	`
+
+	rows, err := r.db.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var subscriptions []*model.Subscription
+	for rows.Next() {
+		subscription := &model.Subscription{}
+		err := rows.Scan(
+			&subscription.ID,
+			&subscription.Symbol,
+			&subscription.Category,
+			&subscription.LookBackDays,
+			&subscription.TriggerLimit,
+			&subscription.LastAlertedAt,
+			&subscription.CreatedAt,
+		)
+		if err != nil {
+			return nil, err
+		}
+		subscriptions = append(subscriptions, subscription)
+	}
+
+	return subscriptions, nil
+}
+
+func (r *SqliteSubscriptionRepository) FindBySymbol(symbol string) (*model.Subscription, error)  {
+	query := `
+		SELECT id, symbol, category, look_back_days, trigger_limit, last_alerted_at, created_at 
+		FROM subscriptions
+		WHERE symbol = ?
+	`
+
+	subscription := &model.Subscription{}
+	err := r.db.QueryRow(query, symbol).Scan(
+		&subscription.ID,
+		&subscription.Symbol,
+		&subscription.Category,
+		&subscription.LookBackDays,
+		&subscription.TriggerLimit,
+		&subscription.LastAlertedAt,
+		&subscription.CreatedAt,
+	)
+
+	if err == sql.ErrNoRows {
+		return nil, errors.New("subscription not found")
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	return subscription, nil
+}
+
+func (r *SqliteSubscriptionRepository) Delete(symbol string) error  {
+	 _, err := r.FindBySymbol(symbol)
+	if err != nil {
+		return err
+	}
+
+	query := `
+		DELETE FROM subscriptions
+		WHERE symbol = ?
+	`
+	result, err := r.db.Exec(query, symbol)
+	if err != nil {
+		return err
+	}
+
+	rowAffected, err := result.RowsAffected()
     if err != nil {
-        log.Fatal(err)
+        return err
+    }
+    if rowAffected == 0 {
+        return errors.New("subscription not found")
     }
 
-	stmt, err := db.Prepare("DELETE FROM subscriptions WHERE id = ?")
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer stmt.Close()
-
-    stmt.Exec(ID)
+	return nil
 }
